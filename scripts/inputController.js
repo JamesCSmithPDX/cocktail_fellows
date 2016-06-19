bf034e636fe83ab84c04f8c664c333a3381fb21f
JamesCSmithPDX <james.c.smithpdx@gmail.com>
2016-06-19 14:17:35 -0700

    merged Sean inputController code

--- scripts/inputController.js
+++ scripts/inputController.js
@@ -1,21 +1,66 @@
 (function(module) {
   var inputController = {};
 
-  var $timeofDay = null;
-  var $alcoholPreference = null;
-  var $skill = null;
-  var $taste = null;
+  function Drink(opts) {
+    Object.keys(opts).forEach(function(e, index, keys) {
+      this[e] = opts[e];
+    },this);
+  };
 
-  inputController.storeSelection = function() {
-    $('select').change(function() {
-      $timeofDay = $('#time').val();
-      $alcoholPreference = $('#preference').val();
-      $skill = $('#skill').val();
-      $taste = $('#taste').val();
+  Drink.all = [];
+
+  inputController.$valueSelect = null;
+
+
+  // inputController.storeSelection = function() {
+  $('select').change(function() {
+    $timeofDay = $('#time').val();
+    inputController.$valueSelect = $(this).val();
+    Drink.fetch(inputController.$valueSelect);
+  });
+  //};
+
+  Drink.fetch = function(queryType) {
+    $.ajax({
+      type: 'GET',
+      url: '/drinks/' + queryType,
+      success: function(data, message, xhr){
+        console.log(data);
+        localStorage.setItem('drinks', JSON.stringify(data));
+        Drink.loadAll(data);
+      },
+      error: function(xhr, status, error){
+        console.log('inside fetchAll ajax error');
+        console.log('ajax error', {xhr: xhr, status: status, error: error});
+      }
     });
   };
+
+  Drink.loadAll = function(data){
+    data.drinks.forEach(function(drink){
+      Drink.all.push(drink);
+      Drink.all.forEach(function(drink){
+        console.log(drink);
+        var newDrink = new Drink(drink);
+      });
+    });
+  };
+
+  // Drink.getAll = function(next) {
+  //   if(localStorage.drinks){
+  //     Drinks.all
+  //   }
+  // };
+
+
 
   inputController.index = function() {};
 
   module.inputController = inputController;
 })(window);
+
+
+//setup Drink.all array
+
+
+//ask the server to make the ajax call
