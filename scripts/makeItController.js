fb5c847686223fc041842381c9e455bd6d5f2456
seanloughran <seancloughran@gmail.com>
2016-06-19 16:31:37 -0700

    Ajax call selected drink by id.

--- scripts/makeItController.js
+++ scripts/makeItController.js
@@ -1,6 +1,21 @@
 (function(module) {
   var makeitController = {};
 
+  makeitController.drinkPlaceHold = '';
+
+  $('#testbtn').click(function() {
+    makeitController.drinkPlaceHold = Drink.all[0];
+
+    $.ajax({
+      url: '/drinks/' + 'lookup.php?i=' + makeitController.drinkPlaceHold.idDrink,
+      success: function(drink) {
+        makeitController.drinkPlaceHold = drink.drinks[0];
+        console.log('Drink name =', makeitController.drinkPlaceHold.strDrink);
+      }
+    });
+  });
+
+
   makeitController.index = function(){};
 
   module.makeitController = makeitController;
